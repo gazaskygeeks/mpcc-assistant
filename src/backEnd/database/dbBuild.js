2dfc394cc8ac66d36ef454a66860020ad81477d7
Noureldean <Noureldean.saed@gmail.com>
2018-02-05 12:46:34 +0200

    modified build.sql and seperated create table commands from testing commands

--- src/backEnd/database/dbBuild.js
+++ src/backEnd/database/dbBuild.js
@@ -3,11 +3,14 @@ const path = require('path');
 const connection = require('./dbConnection');
 
 const buildScripts = fs.readFileSync(path.join(__dirname, 'build.sql')).toString();
+const testsScripts = fs.readFileSync(path.join(__dirname, 'test.sql')).toString();
 
-connection.query(buildScripts, (err, res) => {
-  if (err) {
-    return err;
-  }
-  connection.end();
-  return console.log('Database have been built.', res);
+connection.query(buildScripts, (buildErr, buildRes) => {
+  if (buildErr) return buildErr;
+  console.log('Database have been built.');
+  connection.query(testsScripts, (testsErr, testsRes) => {
+    if(testsErr) return testsErr;
+    console.log('Tests queries put.');
+    connection.end();
+  })
 });
