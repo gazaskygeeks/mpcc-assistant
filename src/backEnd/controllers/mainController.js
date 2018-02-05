209a897db02f379a884d0c5a6be6de9e4b382679
salam <salamdalloul93@gmail.com>
2018-02-05 16:14:16 +0200

    Mentor Panel back-end Routes #17

--- src/backEnd/controllers/mainController.js
+++ src/backEnd/controllers/mainController.js
@@ -2,6 +2,18 @@ const express = require('express');
 
 const router = express.Router();
 
+const getEmail = require('./getEmail');
+const getSingleMentor = require('./getSingleMentor');
+const postEmail = require('./postEmail');
+const updateMentorField = require('./updateMentorField');
+const postDocStatus = require('./postDocStatus');
+
 router.get('/', (req, res) => res.send({ app: 'A' }));
+
+router.get('/dashboard/mentor-panel/:mentorSelector', getSingleMentor);
+router.get('/dashboard/mentor-panel/:mentorSelector/sendEmail/:someEmail', getEmail);
+router.post('/dashboard/mentor-panel/:mentorSelector/sendEmail/:someEmail/confirm', postEmail);
+router.post('/dashboard/mentor-panel/:mentorSelector/edit/:someField', updateMentorField);
+router.post('/dashboard/mentor-panel/:mentorSelector/check/:someDocument', postDocStatus);
 
 module.exports = router;
