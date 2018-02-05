4f61ebf55c25198883950e71522843bb2df0ad8b
Noureldean <Noureldean.saed@gmail.com>
2018-02-05 12:47:40 +0200

    modified build.sql and seperated create table commands from testing commands

--- src/backEnd/database/build.sql
+++ src/backEnd/database/build.sql
@@ -27,8 +27,4 @@ CREATE TABLE forms (
   vars VARCHAR
 );
 
-INSERT INTO users (username, email, password, role) VALUES ('Someone', 'Someone@something', 'Somethingsmart', 'SUPERMAN');
-INSERT INTO mentors (first_name, last_name, email, date_of_arrival, current_stage) VALUES ('Smart', 'Smarter', 'Smart@smart', TO_DATE('17/12/2015', 'DD/MM/YYYY'), 'ALMOST');
-INSERT INTO forms (form_name, vars) VALUES ('Form name', 'the vars');
-SELECT * FROM users where username='Someone';
 COMMIT;
