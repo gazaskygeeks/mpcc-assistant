508d28873d7707cf168d4ea718667b82f18c5969
salam <salamdalloul93@gmail.com>
2018-02-05 15:42:42 +0200

    Dashboard back-end #13

--- src/backEnd/controllers/getMentors.js
+++ src/backEnd/controllers/getMentors.js
@@ -1,6 +1,6 @@
 const selectMentors = require('../database/queries/selectMentors');
 
-exports.get = (req, res, next) => {
+module.exports = (req, res, next) => {
   selectMentors((err, data) => {
     if (err) return next(err);
     return res.status(200).send({
--- src/backEnd/controllers/mainController.js
+++ src/backEnd/controllers/mainController.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const getMentors = require('./getMentors');
 
 router.get('/', (req, res) => res.send({ app: 'A' }));
-router.get('/get-mentors', getMentors.get);
+router.get('/get-mentors', getMentors);
 
 module.exports = router;
