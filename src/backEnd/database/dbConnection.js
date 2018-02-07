8db7d00a75f8f06dcf484a109d49a80706de3485
sultanassi95 <mrsultanassi@gmail.com>
2018-02-07 10:03:15 +0200

    travis-setup Fix #7

--- src/backEnd/database/dbConnection.js
+++ src/backEnd/database/dbConnection.js
@@ -1,13 +1,13 @@
 const { Pool } = require('pg');
 require('env2')('./config.env');
 
-const dbUrl = process.env.DB_URL_LOCAL;
+const dbUrl = process.env.DB_TEST_REMOTE;
 
 if (!dbUrl) throw new Error('Ereror connecting to database');
 
 const pool = new Pool({
   connectionString: dbUrl,
-  ssl: true,
+  ssl: true
 });
 
 module.exports = pool;
