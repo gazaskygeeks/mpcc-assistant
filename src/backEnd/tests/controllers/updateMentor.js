5c6e13f074a7465286297b96e1d730255fe88824
salam <salamdalloul93@gmail.com>
2018-02-07 19:44:03 +0200

    update mentor route test

---
+++ src/backEnd/tests/controllers/updateMentor.js
@@ -0,0 +1,29 @@
+const supertest = require('supertest');
+const app = require('../../app');
+const test = require('tape');
+
+const updateMentorTest = () => {
+  test('test get /dashboard/mentor-panel/Smart-Smarter-1/edit/bio', t => {
+    supertest(app)
+      .post('/dashboard/mentor-panel/Smart-Smarter-1/edit/bio')
+      .send({
+        bio: 'new bio'
+      })
+      .set('Accept', 'application/json, text/plain, */*')
+      .set('Content-Type', 'application/json')
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          console.log(err);
+          t.end();
+        }
+        t.equal(res.statusCode, 200, 'statusCode should equal 200');
+        t.equal(res.type, 'application/json', 'should return res.type application/json');
+        t.equal(res.body.msg, 'MENTOR_UPDATED', 'should return MENTOR_UPDATED');
+        t.end();
+      });
+  });
+};
+module.exports = {
+  updateMentorTest
+};
--- src/backEnd/tests/tests.js
+++ src/backEnd/tests/tests.js
@@ -3,9 +3,11 @@ const { getMentorsTest } = require('./controllers/getMentors');
 const { selectSingleMentorTest } = require('./database/selectSingleMentor');
 const { getSingleMentorTest } = require('./controllers/getSingleMentor');
 const { getEmailTest } = require('./controllers/getEmail');
+const { updateMentorTest } = require('./controllers/updateMentor');
 
 selectMentorsTest();
 getMentorsTest();
 selectSingleMentorTest();
 getSingleMentorTest();
 getEmailTest();
+updateMentorTest();
