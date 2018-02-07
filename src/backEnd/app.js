c1969c09e7994c1df53a81836d5ec6e143f99e85
salam <salamdalloul93@gmail.com>
2018-02-07 20:05:26 +0200

    fixed issue #36

--- src/backEnd/app.js
+++ src/backEnd/app.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, '..', '..', 'public')));
 
 app.use(controllers);
 
-app.use('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, '..', '..', 'public', 'index.html'));
 });
 
