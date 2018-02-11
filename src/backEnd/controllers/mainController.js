98ab574f2cf1c95f1c216306e5e957a18924f5e2
Noureldean <Noureldean.saed@gmail.com>
2018-02-11 09:42:56 +0200

    dots on sendEmail branch

--- src/backEnd/controllers/mainController.js
+++ src/backEnd/controllers/mainController.js
@@ -1,19 +1,21 @@
 const express = require('express');
 
 const router = express.Router();
-const getMentors = require('./getMentors');
-const updateMentorField = require('./updateMentorField');
 const getEmail = require('./getEmail');
-const getSingleMentor = require('./getSingleMentor');
+const sendEmail = require('./sendEmail');
 const postLogin = require('./postLogin');
+const getMentors = require('./getMentors');
 const postDocStatus = require('./postDocStatus');
+const getSingleMentor = require('./getSingleMentor');
+const updateMentorField = require('./updateMentorField');
 
 router.get('/', (req, res) => res.send({ app: 'A' }));
 router.get('/get-mentors', getMentors);
 router.get('/dashboard/mentor-panel/:mentorSelector', getSingleMentor);
-router.post('/dashboard/mentor-panel/:mentorSelector/edit/:fieldType', updateMentorField);
 router.get('/dashboard/mentor-panel/:mentorSelector/check/:docType', postDocStatus);
 router.get('/dashboard/mentor-panel/:mentorSelector/send-email/:emailType', getEmail);
+router.post('/dashboard/mentor-panel/:mentorSelector/edit/:fieldType', updateMentorField);
+router.post('/dashboard/mentor-panel/:mentorSelector/sendEmail/:emailTypel/confirm', sendEmail);
 router.post('/login', postLogin);
 
 module.exports = router;
