fcb2b2dd7a812cb8043ff29ee253f879ff7e7296
Sultan H <mrsultanassi@gmail.com>
2018-02-11 16:27:44 +0200

    Update postLogin.js

--- src/backEnd/controllers/postLogin.js
+++ src/backEnd/controllers/postLogin.js
@@ -1,30 +1,67 @@
-const loginQuery = require('../database/queries/selectUser');
-const bcryptjs = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 
-module.exports = (req, res, next) => {
-  const { username, password } = req.body;
-  loginQuery(username, (err, data) => {
-    if (err) return next(err);
-    else if (data.rowCount === 0) {
-      return res.send({ msg: 'USERNAME/PASSWORD is Incorrect', loggedin: false });
-    }
-    bcryptjs.compare(password, data.rows[0].password, (errorComparing, result) => {
-      if (errorComparing) return next(errorComparing);
-      else if (!result)
-        return res.send({ msg: 'USERNAME/PASSWORD is Incorrect', loggedin: false });
-      const payload = {
-        id: data.rows[0].id,
-        username: data.rows[0].username,
-        role: data.rows[0].role
-      };
-      jwt.sign(payload, process.env.SECRET, (errCreatingToken, token) => {
-        if (errCreatingToken) return next(errCreatingToken);
-        res.setHeader('Set-Cookie',
-          ['logged_in=true; Max-Age=24*60*60', `token=${token}; Max-Age=24*60*60`]);
+import React, { Component } from 'react';
+import { Button, Form } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
 
-        return res.send({ msg: 'LOGGED_IN_SUCCESSFULLY', loggedin: true });
-      });
-    });
-  });
+import './Login.css';
+class Login extends Component {
+  constructor() {
+    super();
+    this._handleInputChange = this._handleInputChange.bind(this);
+    this._handleSubmit = this._handleSubmit.bind(this);
+  }
+  _handleInputChange(e, { name, value }) {
+    const { handleInputChange } = this.props;
+    handleInputChange(name, value);
+  }
+  _handleSubmit() {
+    const { username, password, postLogin } = this.props;
+    postLogin(username, password);
+  }
+
+  render() {
+    const { loginResult } = this.props;
+
+    return (
+      <div className='login'>
+
+        <Form className='form-signin'>
+          <Form.Field>
+            <label>Username:</label>
+            <Form.Input type='text'
+              name='username'
+              onChange={this._handleInputChange}
+              placeholder='username'/>
+          </Form.Field>
+          <Form.Field>
+            <label>Password:</label>
+            <Form.Input type='text' onChange={this._handleInputChange}
+              name='password' type='password' placeholder='password'/>
+          </Form.Field>
+          {!loginResult.loggedin &&
+            <div className='err-msg'>
+              <span>{loginResult.msg}</span>
+            </div>
+          }
+          <Button
+            onClick={this._handleSubmit}
+            type='submit'>Submit</Button>
+        </Form>
+        {loginResult.loggedin? this.props.history.push('/dashboard'):null}
+      </div>
+    );
+  }
+}
+
+Login.propTypes = {
+  handleInputChange: PropTypes.func,
+  postLogin: PropTypes.func,
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
+  isFetching: PropTypes.bool,
+  loginResult: PropTypes.object,
+  error: PropTypes.object,
+  history: PropTypes.object
 };
+
+export default Login;
