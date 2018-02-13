bb870bf4947595c667f623158d7d0fe96cb7956b
Noureldean <Noureldean.saed@gmail.com>
2018-02-13 20:59:13 +0200

    want to pull

--- src/backEnd/controllers/postWaiver.js
+++ src/backEnd/controllers/postWaiver.js
@@ -1,4 +1,21 @@
+const formidable = require('formidable');
+const fs = require('fs');
+const path = require('path');
 module.exports = (req, res, next) => {
-  res.send('Push the waiver back to DB!');
+  console.log('fiiiiiiiile', req.file);
+  const form = new formidable.IncomingForm();
+  console.log(req.body);
+  form.parse(req, (err, fields, files) => {
+    const oldpath = files.filetoupload.path;
+    const newpath = path.join(__dirname, files.filetoupload.name);
+    fs.rename(oldpath, newpath, renameErr => {
+      if (renameErr) throw renameErr;
+      res.write('File uploaded and moved!');
+      console.log('File uploaded and moved!');
+      res.end();
+    });
+    console.log('ok so far');
+    if (err) throw new Error(err);
+  });
   next();
 };
--- src/frontEnd/components/waiver/Waiver.js
+++ src/frontEnd/components/waiver/Waiver.js
@@ -1,14 +1,44 @@
-// import { Modal, Button, Card, Image, Icon, Message } from 'semantic-ui-react';
-import Dropzone from 'react-dropzone';
+import { Form, Modal, Button, Card, Image, Icon, Message } from 'semantic-ui-react';
 import React, { Component } from 'react';
+import Dropzone from 'react-dropzone';
+import axios from 'axios';
+import multer from 'multer';
+import fs from 'fs';
 
 class Waiver extends Component {
+  constructor(props) {
+    super(props);
+    this.onDrop = this.onDrop.bind(this);
+    this.submit = this.submit.bind(this);
+    this.state={
+      waiver: null
+    };
+  }
+
+  onDrop(acceptedFiles, rejectedFiles) {
+    this.setState({ waiver: acceptedFiles });
+  }
+
+  submit() {
+    console.log('I\'m here');
+    const storage = multer.diskStorage({
+      destination: __dirname,
+      filename: (req, file, cb) => {
+        console.log(req);
+        console.log(file);
+      }
+    });
+    axios.post('/submit/signed-waiver/confirm', { file });
+    console.log('I think it had sent, but the server cant parse this');
+  }
 
   render() {
-    console.log(Dropzone.onDrop);
-    console.log(this.props);
     return (
-      <h1>Hello!</h1>
+      <div className='uploader'>
+        <Dropzone onDrop={ files => this.onDrop(files)}>
+        </Dropzone>
+        <Button onClick={this.submit}>Submit</Button>
+      </div>
     );
   }
 }
