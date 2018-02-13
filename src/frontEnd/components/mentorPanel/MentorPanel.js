bf659eb403d7ca4bb9f9abc055e0a3e3cc9f78e9
sultanassi95 <mrsultanassi@gmail.com>
2018-02-13 23:06:31 +0200

    team version #59

--- src/frontEnd/components/mentorPanel/MentorPanel.js
+++ src/frontEnd/components/mentorPanel/MentorPanel.js
@@ -14,35 +14,37 @@ class MentorPanel extends Component {
   }
   render() {
     const { currentMentor, isFetching } = this.props;
-    console.log(currentMentor);
-    // const {
-    //   first_name,
-    //   last_name,
-    //   email,
-    //   date_of_arrival,
-    //   date_of_departure,
-    //   info
-    // } = this.props.currentMentor;
-    // const {
-    //   bio,
-    //   purpose_of_visit,
-    //   photo,
-    //   permission_email_status,
-    //   permit_request_status,
-    //   checklist_form_status,
-    //   signed_waiver_status,
-    //   fligh_information_status,
-    //   mentor_schedule_status,
-    //   permit_approval_status,
-    //   permit_number,
-    //   book_hotel_status,
-    //   schedule_inform_status,
-    //   schedule_approval_status,
-    //   schedule_to_google_status,
-    //   whatsapp_group_status,
-    //   share_final_files_status,
-    //   thanks_email
-    // } = info;
+    console.log(this.props);
+    if (currentMentor.id) {
+      const {
+        first_name,
+        last_name,
+        email,
+        date_of_arrival,
+        date_of_departure,
+        info
+      } = currentMentor;
+      const {
+        bio,
+        purpose_of_visit,
+        photo,
+        permission_email_status,
+        permit_request_status,
+        checklist_form_status,
+        signed_waiver_status,
+        fligh_information_status,
+        mentor_schedule_status,
+        permit_approval_status,
+        permit_number,
+        book_hotel_status,
+        schedule_inform_status,
+        schedule_approval_status,
+        schedule_to_google_status,
+        whatsapp_group_status,
+        share_final_files_status,
+        thanks_email
+      } = info;
+    }
 
     return (
       <div>
@@ -63,56 +65,56 @@ class MentorPanel extends Component {
               <div className='stages-container'>
                 <div className='stage-holder'>
                   <StageHead headTitle='Permit Email:' />
-                  <Editable editableTitle='Bio:' defaultContent='{bio}'/>
-                  <Editable editableTitle='Purpose Of Visit:' defaultContent='{purpose_of_visit}'/>
+                  <Editable editableTitle='Bio:' defaultContent={bio}/>
+                  <Editable editableTitle='Purpose Of Visit:' defaultContent={purpose_of_visit}/>
                 </div>
                 <div className='stage-holder'>
                   <StageHead headTitle='Permission Email:' />
-                  <Status statusTitle='Permission Status:' statusValue='{permission_email_status}'/>
+                  <Status statusTitle='Permission Status:' statusValue={permission_email_status}/>
                 </div>
                 <div className='stage-holder'>
                   <StageHead headTitle='Permit Request Email:' />
-                  <Status statusTitle='Request Status:' statusValue='{permit_request_status}'/>
+                  <Status statusTitle='Request Status:' statusValue={permit_request_status}/>
                 </div>
                 <div className='stage-holder'>
                   <StageHead headTitle='Checklist Email:' />
-                  <Status statusTitle='Checklist Status:' statusValue='{checklist_form_status}'/>
-                  <Status statusTitle='Signed Waiver Status:' statusValue='{signed_waiver_status}'/>
-                  <Status statusTitle='Flight Information Status:' statusValue='{fligh_information_status}'/>
+                  <Status statusTitle='Checklist Status:' statusValue={checklist_form_status}/>
+                  <Status statusTitle='Signed Waiver Status:' statusValue={signed_waiver_status}/>
+                  <Status statusTitle='Flight Information Status:' statusValue={fligh_information_status}/>
                 </div>
                 <div className='stage-holder'>
                   <StageHead headTitle='Scheduling:' />
-                  <Status statusTitle='Mentor Schedulte Build:' statusValue='{mentor_schedule_status}'/>
+                  <Status statusTitle='Mentor Schedulte Build:' statusValue={mentor_schedule_status}/>
                 </div>
                 <div className='stage-holder'>
                   <StageHead headTitle='Permit Approval:' />
-                  <Status statusTitle='Approval Status:' statusValue='{permit_approval_status}'/>
-                  <Status statusTitle='Permission Status:' statusValue='{permit_number}'/>
+                  <Status statusTitle='Approval Status:' statusValue={permit_approval_status}/>
+                  <Status statusTitle='Permission Status:' statusValue={permit_number}/>
                 </div>
                 <div className='stage-holder'>
                   <StageHead headTitle='Book Hotel:' />
-                  <Status statusTitle='Booking Status:' statusValue='{book_hotel_status}'/>
+                  <Status statusTitle='Booking Status:' statusValue={book_hotel_status}/>
                 </div>
                 <div className='stage-holder'>
                   <StageHead headTitle='Inform Mentor:' />
-                  <Status statusTitle='Informed:' statusValue='{schedule_inform_status}'/>
-                  <Status statusTitle='Approval Status:' statusValue='{schedule_approval_status}'/>
+                  <Status statusTitle='Informed:' statusValue={schedule_inform_status}/>
+                  <Status statusTitle='Approval Status:' statusValue={schedule_approval_status}/>
                 </div>
                 <div className='stage-holder'>
                   <StageHead headTitle='Transfer Schedule:' />
-                  <Check checkTitle='Schduled on Google Calender' checkStatus='{schedule_to_google_status}'/>
+                  <Check checkTitle='Schduled on Google Calender' checkStatus={schedule_to_google_status}/>
                 </div>
                 <div className='stage-holder'>
                   <StageHead headTitle='Whatsapp Group:' />
-                  <Check checkTitle='Whatsapp Group Created' checkStatus='{whatsapp_group_status}'/>
+                  <Check checkTitle='Whatsapp Group Created' checkStatus={whatsapp_group_status}/>
                 </div>
                 <div className='stage-holder'>
                   <StageHead headTitle='Share Final Files:' />
-                  <Check checkTitle='Files Share Completed' checkStatus='{share_final_files_status}'/>
+                  <Check checkTitle='Files Share Completed' checkStatus={share_final_files_status}/>
                 </div>
                 <div className='stage-holder'>
                   <StageHead headTitle='Thanks Email:' />
-                  <Status statusTitle='Sent' statusValue='{thanks_email}'/>
+                  <Status statusTitle='Sent' statusValue={thanks_email}/>
                 </div>
               </div>
             </div>
@@ -130,7 +132,8 @@ class MentorPanel extends Component {
 MentorPanel.propTypes = {
   match: PropTypes.object,
   currentMentor: PropTypes.object,
-  isFetching: PropTypes.bool
+  isFetching: PropTypes.bool,
+  getCurrentMentor: PropTypes.func
 };
 
 export default MentorPanel;
