4d128629b66f6a83462eb866e4928bcd638a4998
sultanassi95 <mrsultanassi@gmail.com>
2018-02-17 19:15:15 +0200

    only simple fixes

--- src/backEnd/database/queries/updateMentor.js
+++ src/backEnd/database/queries/updateMentor.js
@@ -8,11 +8,11 @@ const updateMentorField = (mentorObj, cb) => {
     first_name=$1, last_name=$2,
     email=LOWER($3),
     date_of_arrival=$4,
-    date_of_birth=$5,
+    date_of_departure=$5,
     current_stage=$6,
     info=$7
     where id=$8 returning *`,
-    values: [first_name, last_name, email, date_of_arrival, date_of_birth, current_stage, info, id]
+    values: [first_name, last_name, email, date_of_arrival, date_of_departure, current_stage, info, id]
   };
   dbConnection.query(query, cb);
 };
