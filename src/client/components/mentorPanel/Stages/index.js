aa6585ede95076dc83f88987840e8eec75d176d9
sultanassi95 <mrsultanassi@gmail.com>
2018-03-14 17:16:14 +0200

    mentor panel updated #95

--- src/client/components/mentorPanel/Stages/index.js
+++ src/client/components/mentorPanel/Stages/index.js
@@ -2,5 +2,7 @@ import StageHead from './StageHead';
 import Editable from './Editable';
 import Status from './Status';
 import Check from './Check';
+import ParaLink from './ParaLink';
+import Para from './Para';
 
-export { StageHead, Editable, Status, Check };
+export { StageHead, Editable, Status, Check, ParaLink, Para };
