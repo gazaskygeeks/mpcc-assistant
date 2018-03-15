aad7617e7570ae67fb65c61ac949ec9602ce3755
salam <salamdalloul93@gmail.com>
2018-03-15 15:02:59 +0200

    changes as requested #94

--- src/client/components/mentorProfile/info.js
+++ src/client/components/mentorProfile/info.js
@@ -3,25 +3,8 @@ import { Container } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 
 class Info extends Component {
-  constructor() {
-    super();
-    this._handleInputChange = this._handleInputChange.bind(this);
-    this._postField = this._postField.bind(this);
-  }
-
-  _handleInputChange(e, { name, value }) {
-    const { handleInputChange } = this.props;
-    handleInputChange(name, value);
-  }
-
-  _postField() {
-    const { fieldType, fieldValue, postField, mentorID } = this.props;
-    postField(fieldType.replace(/:/g, ''), fieldValue, mentorID);
-  }
-
   render() {
     const { editableTitle, defaultContent } = this.props;
-
     return (
       <div className='info__stage-box'>
         <div className='info__editable__left'>
@@ -44,14 +27,7 @@ class Info extends Component {
 
 Info.propTypes = {
   editableTitle: PropTypes.string,
-  defaultContent: PropTypes.string,
-  handleInputChange: PropTypes.func,
-  fieldType: PropTypes.string,
-  fieldValue: PropTypes.string,
-  postField: PropTypes.func,
-  mentorID: PropTypes.number,
-  isFetching: PropTypes.bool,
-  postFieldResponse: PropTypes.object,
-  error: PropTypes.string
+  defaultContent: PropTypes.string
+
 };
 export default Info;
